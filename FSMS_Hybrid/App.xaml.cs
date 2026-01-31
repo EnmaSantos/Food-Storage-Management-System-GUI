123ed495c92a529eef55656c89b05a657fbaab21
Enmanuel De los Santos Cruz <del20047@byui.edu>
2026-01-30 18:24:48 -0700

    feat: Initial scaffold of FSMS_Hybrid with Supabase and ZXing dependencies

---
+++ FSMS_Hybrid/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace FSMS_Hybrid;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage()) { Title = "FSMS_Hybrid" };
+	}
+}
---
+++ FSMS_Hybrid/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FSMS_Hybrid;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
